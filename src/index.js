ab023e46d3d6112a322eb515874426d7bc63e6ac
Andres Alvarez <aalvarezqu@gmail.com>
2025-01-17 02:09:42 -0500

    add routes from auth

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 import express from 'express'
-import 'dotenv/config'
+import dotenv from 'dotenv'
+import authRoutes from './routes/auth.js'
 import productsRoutes from './routes/products.js'
 import categoriesRoutes from './routes/categories.js'
 import usersRoutes from './routes/users.js'
 import ordersRoutes from './routes/orders.js'
 
+dotenv.config()
+
 const app = express()
+const PORT = process.env.PORT ?? 3000
 
 app.use(express.json())
 
+app.use('/api', authRoutes)
 app.use('/api', productsRoutes)
 app.use('/api', categoriesRoutes)
 app.use('/api', usersRoutes)
 app.use('/api', ordersRoutes)
 
-app.listen(3000)
-console.log('Server on port ', 3000)
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
