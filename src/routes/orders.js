e417483f1daaaca404146bf3923c32d6ec25a678
Andres Alvarez <aalvarezqu@gmail.com>
2025-01-17 22:24:51 -0500

    update Orders routes with auth

--- src/routes/orders.js
+++ src/routes/orders.js
@@ -1,11 +1,12 @@
 import { Router } from "express"
 import { PrismaClient } from "@prisma/client"
+import { authenticateJWT, autorizationUser } from "../middleware/authMiddleware.js"
 
 const router = Router()
 const prisma = new PrismaClient()
 
-// GET ALL ORDERS 
-router.get('/orders', async (req, res) => {
+// GET ALL ORDERS - ADMIN
+router.get('/orders', authenticateJWT, autorizationUser('admin'), async (req, res) => {
   try {
     const orders = await prisma.order.findMany({
       orderBy: {
@@ -19,8 +20,8 @@ router.get('/orders', async (req, res) => {
   }
 })
 
-// GET ORDER BY ID
-router.get('/orders/:id', async (req, res) => {
+// GET ORDER BY ID - USER
+router.get('/orders/:id', authenticateJWT, async (req, res) => {
   try {
     const { id } = req.params
     const order = await prisma.order.findUnique({
@@ -36,6 +37,7 @@ router.get('/orders/:id', async (req, res) => {
 })
 
 // POST AN ORDER
+// Hay que verificar porque la ruta de pago debería guardar la orden
 router.post('/orders', async (req, res) => {
   const { userId, amount, status, address, order_details } = req.body
   try {
@@ -83,10 +85,25 @@ router.post('/orders', async (req, res) => {
   }
 })
 
-// UPDATE AN ORDER
-router.patch('/orders/:id', async (req, res) => {
+// UPDATE AN ORDER - USER OR ADMIN
+router.patch('/orders/:id', authenticateJWT, autorizationUser('user', 'admin'), async (req, res) => {
+  const orderId = req.params.id
+  const { id, role } = req.user
   try {
-    const { id } = req.params
+    const order = prisma.order.findUnique({
+      where: {
+        id: orderId
+      }
+    })
+
+    if (!order) return res.status(404).json({ message: 'Order not found' })
+
+    if (role === 'user') {
+      if (req.body.status !== 'canceled') {
+        return res.status(400).json({ message: 'Invalid status received' })
+      }
+    }
+
     const updateOrder = await prisma.order.update({
       where: {
         id: id
@@ -97,22 +114,6 @@ router.patch('/orders/:id', async (req, res) => {
   } catch (error) {
     console.log(error)
     res.status(500).json({ message: 'Error updating order' })
-  }
-})
-
-// DELETE AN ORDER
-router.delete('/orders/:id', async (req, res) => {
-  try {
-    const { id } = req.params
-    const deleteOrder = await prisma.order.delete({
-      where: {
-        id: id
-      }
-    })
-    res.json(deleteOrder)
-  } catch (error) {
-    console.log(error)
-    res.status(500).json({ message: 'Error delete order' })
   }
 })
 
