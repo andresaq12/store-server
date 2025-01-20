882b0b5c12c32f072752def612c89a34228b1f80
Andres Alvarez <aalvarezqu@gmail.com>
2025-01-19 21:59:38 -0500

    add passport-google and cors in index

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import passport from 'passport'
+import cors from 'cors'
 import authRoutes from './routes/auth.js'
 import productsRoutes from './routes/products.js'
 import categoriesRoutes from './routes/categories.js'
 import usersRoutes from './routes/users.js'
 import ordersRoutes from './routes/orders.js'
+import { configureGoogleStrategy } from './passport/passportConfig.js'
 
 dotenv.config()
+const PORT = process.env.PORT ?? 3000
+const corsOptions = {
+  origin: 'http://localhost:4200',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+};
 
 const app = express()
-const PORT = process.env.PORT ?? 3000
 
+app.use(cors(corsOptions))
 app.use(express.json())
+app.use(passport.initialize())
+
+configureGoogleStrategy(passport)
 
 app.use('/api', authRoutes)
 app.use('/api', productsRoutes)
